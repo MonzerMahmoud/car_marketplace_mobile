8ac9749a93bec21259d75e7c9c33f6d0b146b7d9
MonzerMahmoud <monzermahmoud97@gmail.com>
2024-01-05 17:31:30 +0400

    init commit

---
+++ lib/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Car Marketplace",
+      home: Center(child: Text('Car Marketplace')),
+    );
+  }
+}
